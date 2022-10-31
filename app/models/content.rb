69ef955caa7bab7c097e7d73e513b20bfe3e16d7
nississippi <108444184+nississippi@users.noreply.github.com>
2022-10-31 12:11:34 +0000

    投票機能実装中、contentモデルにメソッド追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,8 @@ class Content < ApplicationRecord
   has_many :ng_tagging_contents, dependent: :destroy
   has_many :ng_expressions, through: :ng_tagging_contents
   has_many :ng_answers, dependent: :destroy
+
+  def answered_by?(customer, ng_expression, vote)
+    ng_answers.exists?(customer_id: customer.id, ng_expression_id: ng_expression.id, vote: vote)
+  end
 end
