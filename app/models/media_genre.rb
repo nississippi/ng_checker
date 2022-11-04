1334a6459f486f9116cd79f53673078c09ae09e2
nississippi <108444184+nississippi@users.noreply.github.com>
2022-11-04 14:56:02 +0000

    媒体を複数登録できるよう変更

--- app/models/media_genre.rb
+++ app/models/media_genre.rb
@@ -1,4 +1,5 @@
 class MediaGenre < ApplicationRecord
+  #中間テーブル先に記述しないとエラー起こすかも
   has_many :media_tag_contents, dependent: :destroy
   has_many :contents, through: :media_tag_contents
   validates :media, presence: true
