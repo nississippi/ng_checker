1fb85399635e2f05ab5ed3f19f1261eec4462d71
nississippi <108444184+nississippi@users.noreply.github.com>
2022-11-06 13:34:14 +0000

    作品傾向中間テーブル作成

---
+++ app/models/story_tag_content.rb
@@ -0,0 +1,2 @@
+class StoryTagContent < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_124917) do
+ActiveRecord::Schema.define(version: 2022_11_06_133249) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -116,6 +116,15 @@ ActiveRecord::Schema.define(version: 2022_11_06_124917) do
     t.string "story"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "story_tag_contents", force: :cascade do |t|
+    t.integer "story_genre_id"
+    t.integer "content_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_story_tag_contents_on_content_id"
+    t.index ["story_genre_id"], name: "index_story_tag_contents_on_story_genre_id"
   end
 
   create_table "votes", force: :cascade do |t|
