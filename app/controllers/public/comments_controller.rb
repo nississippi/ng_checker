149e19856561f4be9787631b3a155208ecc6b3e8
nississippi <108444184+nississippi@users.noreply.github.com>
2022-11-23 09:46:21 +0000

    下書き機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -11,12 +11,22 @@ class Public::CommentsController < ApplicationController
       content_id: comment_params[:content_id],
       customer_id: current_customer.id,
       comment: comment_params[:comment])
-
-    if @comment.save
-      #クエリパラメータでcontent_idを渡す必要がある
-      redirect_to ng_expression_comments_path(content_id: comment_params[:content_id])
+    # 投稿ボタンを押下した場合
+    if params[:post]
+      if @comment.save
+        #クエリパラメータでcontent_idを渡す必要がある
+        redirect_to ng_expression_comments_path(content_id: comment_params[:content_id])
+      else
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
+      end
+    # 下書きボタンを押下した場合
     else
-      render :new
+      if @comment.update(is_draft: true)
+        redirect_to ng_expression_comments_path(content_id: comment_params[:content_id]),
+        notice: "コメントを下書き保存しました！"
+      else
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
+      end
     end
   end
 
