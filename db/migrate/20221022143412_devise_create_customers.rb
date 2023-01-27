c967701015d8b78758755446af4bd7f5abf5c658
nississippi <108444184+nississippi@users.noreply.github.com>
2023-01-27 15:21:37 +0000

    ユーザーテーブルのマイグレーションファイル修正

--- db/migrate/20221022143412_devise_create_customers.rb
+++ db/migrate/20221022143412_devise_create_customers.rb
@@ -6,6 +6,8 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.boolean :is_deleted, default: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/migrate/20221022151656_add_nickname_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddNicknameToCustomers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :customers, :nickname, :string
-  end
-end
